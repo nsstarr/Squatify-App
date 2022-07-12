14ead04b61c520cd2fad1047364d7a039b27f6fc
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-07-12 08:41:24 +0100

    insight element positioned (50% done)

--- sqautify/src/Components/Insight Element/InsightElement.jsx
+++ sqautify/src/Components/Insight Element/InsightElement.jsx
@@ -1,26 +1,44 @@
 import React from "react";
 import styled from "styled-components";
 
-const InsightCard = styled.div`
-  box-sizing: border-box;
+const InsightTitle = styled.div`
+  font-size: 16px;
+  width: 3em;
+  height: 3em;
 
-  position: absolute;
-  width: 91.9px;
-  height: 84.85px;
-  left: 145px;
-  top: 468px;
-
-  border: 5px solid #1ed760;
-  border-radius: 50px;
+  ${"" /* position: absolute; */}
 
   color: #1ed760;
 `;
 
-const InsightElement = ({children}) => {
+const InsightNumber = styled.div`
+  font-size: 28px;
+  width: 3em;
+  height: 3em;
+  ${"" /* box-sizing: border-box; */}
+
+  ${"" /* position: absolute; */}
+
+  border: 5px solid #1ed760;
+  border-radius: 50px;
+  color: #1ed760;
+`;
+
+const InsightCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-items: center;
+`;
+
+//font weight
+
+const InsightElement = ({ title, number }) => {
   return (
-    <>
-      <InsightCard>{children}</InsightCard>
-    </>
+    <InsightCard>
+      <InsightTitle>{title}</InsightTitle>
+      <InsightNumber>{number}</InsightNumber>
+    </InsightCard>
   );
 };
 
--- sqautify/src/Components/Insight/Insight.jsx
+++ sqautify/src/Components/Insight/Insight.jsx
@@ -4,9 +4,9 @@ import InsightElement from "../Insight Element/InsightElement";
 const Insight = () => {
   return (
     <div>
-      <InsightElement>Squats today</InsightElement>
-      <InsightElement>Squats this week: total</InsightElement>
-      <InsightElement>Squats this week: average</InsightElement>
+      <InsightElement title="squats today" number="15" />
+      <InsightElement title="Squats this week: total" number="150" />
+      <InsightElement title="Squats this week: average" number="20" />
     </div>
   );
 };
